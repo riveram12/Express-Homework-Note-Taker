1e7a630920c42d261f74b24d4187d32c6d159603
Mariah Rivera <rivmariah06@gmail.com>
2021-02-05 12:31:19 -0500

    adding routes folder and .js files for api rputes and html routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+
+module.exports = function(app) {
+
+    app.get("/api/notes", function(req, res) {
+       
+        res.json(data);
+
+    });
+
+    app.get("/api/notes/:id", function(req, res) {
+
+        res.json(data[Number(req.params.id)]);
+
+    });
