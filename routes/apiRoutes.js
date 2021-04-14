6a68357ba91d7ac5693254cd6da1d4b8a971c9cd
Mariah Rivera <rivmariah06@gmail.com>
2021-04-13 22:27:45 -0400

    adding missing packages and fixing routes & index.js files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ const path = require("path")
 const router = express.Router()
 
 router.get("/api/notes", function(req, res) {
-    fs.readFile(path.resolve(__dirname, "../Develop/db/db.json"), "utf8", function(err, data) {
+    fs.readFile(path.resolve(__dirname, "../db/db.json"), "utf8", function(err, data) {
         if (err) throw err
         res.json(JSON.parse(data))
     })
@@ -13,11 +13,11 @@ router.get("/api/notes", function(req, res) {
 router.post("/api/notes", function(req, res) {
     var body = req.body
     console.log("body", body)
-    fs.readFile(path.resolve(__dirname, "../Develop/db/db.json"), "utf8", function(err, data) {
+    fs.readFile(path.resolve(__dirname, "../db/db.json"), "utf8", function(err, data) {
         if (err) throw err
         const currentNotes = JSON.parse(data)
         currentNotes.push(Object.assign({ id: uniqid() }, body))
-        fs.writeFile(path.resolve(__dirname, "../Develop/db/db.json"), JSON.stringify(currentNotes), function(error) {
+        fs.writeFile(path.resolve(__dirname, "../db/db.json"), JSON.stringify(currentNotes), function(error) {
             if (error) throw error
             res.sendStatus(201)
         })
@@ -26,11 +26,11 @@ router.post("/api/notes", function(req, res) {
 
 });
 router.delete("/api/notes/:id", function(req, res) {
-    fs.readFile(path.resolve(__dirname, "../Develop/db/db.json"), "utf8", function(err, data) {
+    fs.readFile(path.resolve(__dirname, "../db/db.json"), "utf8", function(err, data) {
         if (err) throw err
         const currentNotes = JSON.parse(data)
         const newNotes = currentNotes.filter(function(note) { return note.id !== req.params.id; })
-        fs.writeFile(path.resolve(__dirname, "../Develop/db/db.json"), JSON.stringify(newNotes), function(error) {
+        fs.writeFile(path.resolve(__dirname, "../db/db.json"), JSON.stringify(newNotes), function(error) {
             if (error) throw error
             res.sendStatus(204)
         })
